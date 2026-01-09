8daedc4470e64f6ad28b2b39dc71c00fb88d460b
alfredjaeckel <alfred.thomas.jaeckel@gmail.com>
2026-01-09 14:17:07 +0100

    Initial Commit

---
+++ Vibration Threshold/Vibration_ThresholdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibration_ThresholdApp.swift
+//  Vibration Threshold
+//
+//  Created by Alfred on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Vibration_ThresholdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
